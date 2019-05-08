f579b9eab1a8be6cb6c472dcb6c018aaa50e5611
Bloody@DESKTOP-INU2790 <mathieu_franon@outlook.fr>
2019-05-08 19:33:34 +0200

    CSV02/03/04: mise en place du templating

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+	/**
+	 * Accès à la page d'accueil
+	 *
+	 * @Route("/", name="home")
+	 */
+	public function home()
+	{
+		return $this->render('/home.html.twig', ['title' => "Bienvenue"]);
+	}
+
+	/**
+	 * Accès à la page photos
+	 *
+	 * @Route("/photo", name="photo")
+	 */
+	public function photo()
+	{
+		return $this->render('/photo.html.twig');
+	}
+
+	/**
+	 * Accès à la page forum
+	 *
+	 * @Route("/forum", name="forum")
+	 */
+	public function forum()
+	{
+		return $this->render('/forum.html.twig');
+	}
+
+	/**
+	 * Accès à la page connexion
+	 *
+	 * @Route("/connection", name="connection")
+	 */
+	public function connection()
+	{
+		return $this->render('/connection.html.twig');
+	}
+}
